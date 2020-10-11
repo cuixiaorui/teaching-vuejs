f40708f7ee00cdef103ff453eb7947f1f7329ddf
cuixiaorui <cui_xiaorui@126.com>
2020-10-11 17:04:01 +0800

    feat(play-plane): 实现自定义渲染接口逻辑 -03

--- packages/play-plane/src/runtime-canvas/index.js
+++ packages/play-plane/src/runtime-canvas/index.js
@@ -1,7 +1,36 @@
 // 实现基于 canvas 自定义的渲染器
 import { createRenderer } from "vue";
+import { Container } from "pixi.js";
 
-const renderer = createRenderer({});
+const renderer = createRenderer({
+  createElement(type) {
+    let element;
+    switch (type) {
+      case "container":
+        element = new Container();
+        break;
+    }
+
+    return element;
+  },
+
+  insert(el, parent) {
+    if (el) {
+      parent.addChild(el);
+    }
+  },
+  parentNode(node) {
+    return node.parent;
+  },
+  remove(el) {
+    if (el && el.parent) {
+      el.parent.removeChild(el);
+    }
+  },
+
+  nextSibling() {},
+  createComment() {},
+});
 
 export function createApp(rootComponent) {
   return renderer.createApp(rootComponent);
